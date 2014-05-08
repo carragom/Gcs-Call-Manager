e1265294a60d3f8cb700fdab183b500fb4aa865b
Federico Figueroa C <ffc.figueroa@gmail.com>
2014-05-08 12:04:25 -0600

    Set user cookie

---
+++ config/middleware/userCookie.js
@@ -0,0 +1,9 @@
+
+exports.set = function (req, res, next) {
+	if (req.user) {
+		var userInfo = req.user.userInfo;
+		userInfo.name = encodeURI(req.user.userInfo.name);
+		res.cookie('user', JSON.stringify(userInfo));
+	}
+	next();
+}
